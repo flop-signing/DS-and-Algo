69fbe4f546b37a3dc74599f74890ef8a2ef6fd8e
flop-signing <mehedy.mmc272@gmail.com>
2025-08-11 02:22:59 +0600

    Implement Circular LinkedList

---
+++ src/Algorithms/linkedlist/circularlinkedlist/Main.java
@@ -0,0 +1,15 @@
+package Algorithms.linkedlist.circularlinkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CircularLinkedList list=new CircularLinkedList();
+        list.insert(23);
+        list.insert(34);
+        list.insert(2);
+        list.insert(13);
+
+
+        list.display();
+    }
+}
