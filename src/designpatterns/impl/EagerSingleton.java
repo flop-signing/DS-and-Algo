2e192bff7fe9398881f9243430d286389806c8fe
mehedy22 <mehedy.mmc272@gmail.com>
2026-08-03 09:47:19 +0600

    Implement Eager Singleton design pattern

---
+++ src/designpatterns/impl/EagerSingleton.java
@@ -0,0 +1,36 @@
+package designpatterns.impl;
+
+/*
+* Is it thread-safe?
+Yes.
+Why?
+Java guarantees that class initialization happens exactly once, and the JVM synchronizes this process internally.
+If two threads reach:
+Singleton.getInstance();
+
+at the same time and the class hasn't been initialized yet: one thread performs the class initialization,
+ the other waits,
+after initialization, both receive the same instance.
+This guarantee comes from the JVM's class initialization mechanism, so you don't need explicit synchronization here.
+*
+*
+*
+* Eager Singleton solves the "single instance" problem, but introduces a new question:
+What if creating the object is expensive and it may never be used? (e.g a db connection)
+* */
+
+public class EagerSingleton {
+
+    // Instance is created immediately when the class is loaded
+    private static final EagerSingleton INSTANCE = new EagerSingleton();
+
+    // Prevent object creation from outside
+    private EagerSingleton() {
+
+
+    }
+    // Global access point
+    public static EagerSingleton getInstance() {
+        return INSTANCE;
+    }
+}
