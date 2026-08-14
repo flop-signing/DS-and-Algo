1609917010e796a14b37c2b983db7e9e73a4cefa
mehedy22 <mehedy.cuet.cse@gmail.com>
2026-08-14 16:08:19 +0600

    Implement the improved version of Binary Tree

---
+++ src/Algorithms/tree/binarytree/BinaryTreeImproved.java
@@ -0,0 +1,104 @@
+package Algorithms.tree.binarytree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BinaryTreeImproved {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node create() {
+        Node root;
+        Node p, t;
+        int x;
+        Queue<Node> q = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the root Node: ");
+        x = sc.nextInt();
+        root = new Node(x);
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            p = q.poll();
+            System.out.println("Enter left child of " + p.data + " (-1 if none): ");
+            x = sc.nextInt();
+            if (x != -1) {
+                t = new Node(x);
+                p.left = t;
+                q.add(t);
+            }
+            System.out.println("Enter right child of " + p.data + " (-1 if none): ");
+            x = sc.nextInt();
+            if (x != -1) {
+                t = new Node(x);
+                p.right = t;
+                q.add(t);
+            }
+        }
+        return root;
+
+    }
+
+    public static void preorderRecursion(Node temp) {
+        if (temp != null) {
+            System.out.print(temp.data + " ");
+            preorderRecursion(temp.left);
+            preorderRecursion(temp.right);
+        }
+    }
+
+    public static void preorder(Node temp) {
+        Stack<Node> stack = new Stack<>();
+        while (temp != null || !stack.isEmpty()) {
+            if (temp != null) {
+                System.out.println(temp.data + " ");
+                stack.push(temp);
+                temp = temp.left;
+
+            } else {
+                temp = stack.pop();
+                temp = temp.right;
+            }
+        }
+    }
+
+    public static void preorderIterative(Node temp) {
+        Stack<Node> stack = new Stack<>();
+        while (temp != null || !stack.isEmpty()) {
+            if (temp != null) {
+                System.out.print(temp.data + " ");
+                stack.push(temp);
+                temp = temp.left;
+            } else {
+                temp = stack.pop();
+                temp = temp.right;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node root = create();
+
+        System.out.print("\nPreorder (Recursive): ");
+        preorderRecursion(root);
+
+        System.out.print("\nPreorder (Iterative): ");
+        preorderIterative(root);
+    }
+
+
+}
---
+++ src/Algorithms/tree/binarytree/Main.java
@@ -0,0 +1,4 @@
+package Algorithms.tree.binarytree;
+
+public class Main {
+}
