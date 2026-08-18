c5a70aaf46649a9687ee55350565c080d7379ad8
mehedy22 <mehedy.cuet.cse@gmail.com>
2026-08-19 00:16:02 +0600

    Implementing insrtion functionalities in recursive way

--- src/Algorithms/tree/bst/BinarySearchTree.java
+++ src/Algorithms/tree/bst/BinarySearchTree.java
@@ -51,4 +51,19 @@ public class BinarySearchTree {
             inorder(t.right);
         }
     }
+    Node insert(Node root, int key) {
+
+        if (root == null) {
+            return new Node(key);
+        }
+
+        if (key < root.data) {
+            root.left = insert(root.left, key);
+        }
+        else if (key > root.data) {
+            root.right = insert(root.right, key);
+        }
+
+        return root;
+    }
 }
